6742a5005ebd6f47f7c89aa3168c6010d317324d
Hazem Hussin <102246134+HZM35@users.noreply.github.com>
2023-09-19 10:32:14 +0200

    Add files via upload

---
+++ quizapp/quizapp/src/main/java/com/hazem/quizapp/service/QuizService.java
@@ -0,0 +1,52 @@
+package com.hazem.quizapp.service;
+
+import com.hazem.quizapp.dao.QuestionDao;
+import com.hazem.quizapp.dao.QuizDao;
+import com.hazem.quizapp.model.Question;
+import com.hazem.quizapp.model.QuestionWrapper;
+import com.hazem.quizapp.model.Quiz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuizService {
+    @Autowired
+    QuizDao quizDao;
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<String> createQuiz( String category,  int numQ, String title ){
+
+        List< Question> questions = questionDao.findRandomQuestionsByCategory(category, numQ);
+
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestions(questions);
+        quizDao.save(quiz);
+
+        return new ResponseEntity<>("SUCCESS", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper> > getQuizQuestions(Integer id) {
+
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List <Question> questionsFromDB = quiz.get().getQuestions();
+        List <QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for (Question q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+
+    }
+}
